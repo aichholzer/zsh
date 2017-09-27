6f63d528f325c7a79083e5c4045a4f7d71bdf83a
Stefan Aichholzer <theaichholzer@gmail.com>
2017-09-27 10:19:20 +1000

    Theme and installer

---
+++ README.md
@@ -0,0 +1,10 @@
+# ZSH
+
+This theme is based on the [agnoster](https://github.com/agnoster/agnoster-zsh-theme) theme and using [oh my ZSH](https://github.com/robbyrussell/oh-my-zsh) for orchestration.
+
+### Install
+
+* `git clone git@github.com:aichholzer/zsh.git`
+* `cd zsh && ./install.sh`
+* Follow the prompts (if any)
+* Enjoy!
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+echo
+echo Cloning and installing fonts...
+git clone https://github.com/powerline/fonts.git --depth=1 2>/dev/null &
+PID=$!
+LOOP=1
+while kill -0 $PID 2>/dev/null; do
+  printf  "•"
+  sleep 0.03
+  ((LOOP+=1))
+  if [ $LOOP == 32 ]; then
+      sleep 0.5
+      echo -ne "\r\033[0K"
+      LOOP=1
+  fi
+done
+cd fonts && ./install.sh &>/dev/null
+
+echo
+cd .. && rm .zshrc && rm -rf ~/.zsh* ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &
+PID=$!
+while kill -0 $PID 2>/dev/null; do
+  sleep 1
+done
+
+sleep 1
+cp aichholzer.zsh-theme ~/.oh-my-zsh/themes/
+sed -i -e '/ZSH_THEME=/s/=.*/=\"aichholzer\"/' ~/.zshrc
+sed -i -e '/# COMPLETION_WAITING_DOTS/s//COMPLETION_WAITING_DOTS/' ~/.zshrc
+sed -i -e '/plugins=/s/=.*/=\(git npm node nyan\)/' ~/.zshrc
+
+echo "DEFAULT_USER=$(whoami)" >> ~/.zshrc
+source ~/.zshrc 2>/dev/null
+
+cd .. && rm -rf zsh
+
+clear
+echo
+echo 👍🏻
+echo Done, restart your terminal or open a new window.
+echo
